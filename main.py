d2f34e2b945e41ee3ee33fcfe91b6e4f12c90431
krzsandra <krz.sandra@gmail.com>
2022-05-22 15:30:15 +0200

    Initial commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def check_pin():
         clientpin = int(input("W celu wykonania opecaji podaj 4 cyfrowy pin:"))
         trials += 1
         if clientpin == pin:
-            print("Pin Poprawny")
+            print("Pin Poprawny, Wybierz opcje")
             pincorect = True
             break
         elif trials == 3:
@@ -18,11 +18,53 @@ def check_pin():
     return pincorect
 
 
+def option():
+    options = int(
+        input("\nWypłata gotówki wprowadź '1'\nSprawdzenie stanu konta wprowadź '2'\nZakończenie wprowadź '3'\n:"))
+    return options
+
+
+
 #         code here
 
 if __name__ == '__main__':
+    accountbalans = 550
+    atmstate = 1000
     print("Witaj w symulatorze bankomatu ")
     if check_pin():
-        pass
+        options = option()
+        if options == 1:
+            payment = int(input("Proszę wprowadzic kwotę:"))
+            multiple = (payment % 50)
+            if multiple == 0:
+                if payment <= accountbalans:
+                    if payment <= atmstate:
+                        cash200 = payment // 200
+                        cash100 = (payment - (200 * cash200)) // 100
+                        cash50 = (payment - (200 * cash200) - (100 * cash100)) // 50
+                        print(cash200, cash100, cash50)
+                    else:
+                        print("brak środków w atm")
+
+                else:
+                    print("komunikat brak srodkow na koncie")
+
+            else:
+                print("błedna kwota")
+                check_pin()
+
+
+
+
+
+        elif options == 2 :
+            accountbalans = 550
+            print("Twój stan konta wynosi:{accountbalans}".format(accountbalans=accountbalans))
+
+        elif options == 2 :
+            print("dziękujemy zapraszam ponownie")
+        else:
+            print("Nie wybrano poprawnej opcji, zablokowanie")
+
     else:
         print("zablokowany")
