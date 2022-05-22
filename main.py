e2f32c5917fef4860a5a1f7504efb2a8fb4ad68f
krzsandra <krz.sandra@gmail.com>
2022-05-22 19:38:06 +0200

    Added function showing options of transaction for users and function for withdrawal from an ATM

--- main.py
+++ main.py
@@ -23,6 +23,25 @@ def option():
         input("\nWypłata gotówki wprowadź '1'\nSprawdzenie stanu konta wprowadź '2'\nZakończenie wprowadź '3'\n:"))
     return options
 
+def proba():
+    payment = int(input("Proszę wprowadzic kwotę:"))
+    multiple = (payment % 50)
+    if multiple == 0:
+        if payment <= accountbalans:
+            if payment <= atmstate:
+                cash200 = payment // 200
+                cash100 = (payment - (200 * cash200)) // 100
+                cash50 = (payment - (200 * cash200) - (100 * cash100)) // 50
+                print(cash200, cash100, cash50)
+            else:
+                print("brak środków w atm")
+
+        else:
+            print("komunikat brak srodkow na koncie")
+
+    else:
+        print("błedna kwota")
+
 
 
 #         code here
@@ -32,39 +51,30 @@ if __name__ == '__main__':
     atmstate = 1000
     print("Witaj w symulatorze bankomatu ")
     if check_pin():
-        options = option()
-        if options == 1:
-            payment = int(input("Proszę wprowadzic kwotę:"))
-            multiple = (payment % 50)
-            if multiple == 0:
-                if payment <= accountbalans:
-                    if payment <= atmstate:
-                        cash200 = payment // 200
-                        cash100 = (payment - (200 * cash200)) // 100
-                        cash50 = (payment - (200 * cash200) - (100 * cash100)) // 50
-                        print(cash200, cash100, cash50)
-                    else:
-                        print("brak środków w atm")
+        options = 4
+        while options > 3 or options <= 0:
+            options = option()
 
-                else:
-                    print("komunikat brak srodkow na koncie")
+            if options == 1:
+                 proba()
+                 break
+            elif options == 2:
+                accountbalans = 550
+                print("Twój stan konta wynosi:{accountbalans}".format(accountbalans=accountbalans))
+                break
+            elif options == 3:
+                print("dziękujemy zapraszam ponownie")
+                break
 
             else:
-                print("błedna kwota")
-                check_pin()
-
-
-
-
-
-        elif options == 2 :
-            accountbalans = 550
-            print("Twój stan konta wynosi:{accountbalans}".format(accountbalans=accountbalans))
-
-        elif options == 2 :
-            print("dziękujemy zapraszam ponownie")
-        else:
-            print("Nie wybrano poprawnej opcji, zablokowanie")
+                print("Nie wybrano poprawnej opcji, spróbuj raz jeszcze")
 
     else:
         print("zablokowany")
+
+
+
+
+
+
+
