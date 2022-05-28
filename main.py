63c67aefcd9ddb4b3426795b10926bf58b552d34
krzsandra <krz.sandra@gmail.com>
2022-05-28 16:44:09 +0200

    correcting the names of variables and the content of messages

--- main.py
+++ main.py
@@ -5,72 +5,73 @@ def check_pin():
     pincorect = False
     trials = 0
     while trials < 3:
-        clientpin = int(input("W celu wykonania opecaji podaj 4 cyfrowy pin:"))
+        clientpin = int(input("W celu wykonania operacji podaj cztero cyfrowy PIN:"))
         trials += 1
         if clientpin == pin:
-            print("Pin Poprawny, Wybierz opcje")
+            print("PIN poprawny, wybierz operację jaką chcesz wykonać:")
             pincorect = True
             break
         elif trials == 3:
             break
         else:
-            print("spróbuj ponownie")
+            print("PIN niepoprawny, spróbuj ponownie")
     return pincorect
 
 
-def option():
-    options = int(
-        input("\nWypłata gotówki wprowadź '1'\nSprawdzenie stanu konta wprowadź '2'\nZakończenie wprowadź '3'\n:"))
-    return options
+def operation_selection():
+    selectedoperation = int(
+        input("\nWypłata gotówki wprowadź '1'\nWyświetlenie stanu konta wprowadź '2'\nWyjście z SYMULATORA BANKOMATU '3'\n:"))
+    return selectedoperation
 
-def proba():
-    payment = int(input("Proszę wprowadzic kwotę:"))
-    multiple = (payment % 50)
-    if multiple == 0:
-        if payment <= accountbalans:
-            if payment <= atmstate:
-                cash200 = payment // 200
-                cash100 = (payment - (200 * cash200)) // 100
-                cash50 = (payment - (200 * cash200) - (100 * cash100)) // 50
-                print(cash200, cash100, cash50)
+def cash_withdrawal():
+    multiple = 1
+    while multiple > 0:
+        paycheck = int(input("Proszę wprowadzić kwotę do wypłaty:"))
+        multiple = (paycheck % 50)
+        if multiple == 0:
+            if paycheck <= accountbalans:
+                if paycheck <= atmstate:
+                    nrofbanknotes200 = paycheck // 200
+                    nrofbanknotes100 = (paycheck - (200 * nrofbanknotes200)) // 100
+                    nrofbanknotes50 = (paycheck - (200 * nrofbanknotes200) - (100 * nrofbanknotes100)) // 50
+                    print("Wypłacasz kowotę:",paycheck,"\nOtrzymujesz:","\n",nrofbanknotes200,"banknot(ów) o nominale 200 PLN","\n", nrofbanknotes100, "banknot(ów) o nominale 100 PLN", "\n", nrofbanknotes50,"banknot(ów) o nominale 50 PLN")
+
+                else:
+                    print("Brak środków w SYMULATORZE BANKOMATU \nZapraszamy ponownie")
+
             else:
-                print("brak środków w atm")
+                print("Brak środków na koncie \nZapraszamy ponownie")
 
         else:
-            print("komunikat brak srodkow na koncie")
-
-    else:
-        print("błedna kwota")
-
-
+            print("Błędna kwota. Wprowadzona kwota musi być wielokrotnością liczby 50.")
+    return
 
 #         code here
 
 if __name__ == '__main__':
     accountbalans = 550
     atmstate = 1000
-    print("Witaj w symulatorze bankomatu ")
+    print("Witaj w SYMULATORZE BANKOMATU")
     if check_pin():
-        options = 4
-        while options > 3 or options <= 0:
-            options = option()
-
-            if options == 1:
-                 proba()
+        selectedoperation = 4
+        while selectedoperation > 3 or selectedoperation <= 0:
+            selectedoperation = operation_selection()
+            if selectedoperation == 1:
+                 cash_withdrawal()
                  break
-            elif options == 2:
+            elif selectedoperation == 2:
                 accountbalans = 550
                 print("Twój stan konta wynosi:{accountbalans}".format(accountbalans=accountbalans))
                 break
-            elif options == 3:
-                print("dziękujemy zapraszam ponownie")
+            elif selectedoperation == 3:
+                print("Dziękujemy, zapraszamy ponownie")
                 break
 
             else:
-                print("Nie wybrano poprawnej opcji, spróbuj raz jeszcze")
+                print("Nie wybrano poprawnej operacji, spróbuj ponownie:")
 
     else:
-        print("zablokowany")
+        print("Użytkownik zablkowany. Dziękujemy, zapraszamy ponownie")
 
 
 
